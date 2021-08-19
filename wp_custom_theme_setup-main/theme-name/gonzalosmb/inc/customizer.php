2d669004facf6414ab2ecddbc9561f75b251cd40
perrarian <gonzalo.smb1@gmail.com>
2021-08-19 17:03:03 +0200

    wp-theme

---
+++ wp_custom_theme_setup-main/theme-name/gonzalosmb/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Gonzalo Page Theme Customizer
+ *
+ * @package Gonzalo_Page
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function gonzalosmb_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'gonzalosmb_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'gonzalosmb_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'gonzalosmb_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function gonzalosmb_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function gonzalosmb_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function gonzalosmb_customize_preview_js() {
+	wp_enqueue_script( 'gonzalosmb-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'gonzalosmb_customize_preview_js' );
